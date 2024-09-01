5975dc5dee4c91211038b96277eacc90b7a8b780
PARK Yong Seo <j@saro.me>
2024-09-01 23:08:18 +0900

    init

---
+++ src/main/kotlin/me/saro/miniorm/Util.kt
@@ -0,0 +1,27 @@
+package me.saro.miniorm
+
+class Util {
+    companion object {
+
+        @JvmStatic
+        fun camelToUnderscore(text: String): String {
+            val tmp = StringBuilder(text.length * 2)
+            var upper = true
+            for (i in text.indices) {
+                val c = text[i]
+
+                if (c.isUpperCase()) {
+                    if (!upper) {
+                        tmp.append('_')
+                    }
+                    tmp.append(c.lowercaseChar())
+                    upper = true
+                } else {
+                    tmp.append(c)
+                    upper = false
+                }
+            }
+            return tmp.toString()
+        }
+    }
+}
